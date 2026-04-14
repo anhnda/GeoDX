ae61bff238c577eebb345a3637c641f7a147c46e
AnhND <anh@gmail.com>
2026-04-14 14:20:30 +0700

    Update

--- models/physics/__init__.py
+++ models/physics/__init__.py
@@ -3,13 +3,14 @@ Physics-informed modules for molecular conformation generation.
 Implements differentiable quantum mechanics (xTB) for Hamiltonian-Informed Schrödinger Bridge.
 """
 
-from .dxtb_interface import DXTBForceField, compute_xtb_forces, compute_xtb_energy
+from .dxtb_interface import DXTBForceField, compute_xtb_forces, compute_xtb_energy, WarmStartOptimizer
 from .gamma_scheduler import GammaScheduler, get_gamma_schedule
 
 __all__ = [
     'DXTBForceField',
     'compute_xtb_forces',
     'compute_xtb_energy',
+    'WarmStartOptimizer',
     'GammaScheduler',
     'get_gamma_schedule'
 ]
